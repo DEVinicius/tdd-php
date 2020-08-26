627017592813e2c03045c326700a4b2fc685ee5c
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-26 11:00:41 -0300

    test function escope

--- source/Tests/AlunoTest.php
+++ source/Tests/AlunoTest.php
@@ -7,4 +7,27 @@ use Source\App\Aluno;
 class AlunoTest extends Aluno
 {
     //criar funções de Teste para cada função da classe Aluno
+    public function testGetNome()
+    {}
+
+    public function testSetNome()
+    {}
+
+    public function testGetNotas()
+    {}
+
+    public function testSetNotas()
+    {}
+
+    public function testGetNotaGeral()
+    {}
+
+    public function testSetNotaGeral()
+    {}
+
+    public function testGetTurma()
+    {}
+
+    public function testSetTurma()
+    {}
 }
